dce82228daf4fabefac4a17c9f02fa0c26f0dfa2
Matthew Bascom <matt.bijan@gmail.com>
2022-11-03 10:01:32 -0500

    created htmlRoutes and Notes routes

---
+++ routes/notes.js
@@ -0,0 +1,30 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new UX/UI tip
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { noteTitle, noteText } = req.body;
+
+  if (req.body) {
+    const newTip = {
+      noteTitle,
+      noteText,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+module.exports = notes;
